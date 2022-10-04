74864ba30ae3259ffe6ccabbf21dca002a8eb778
Crealy <82323759+Crealy@users.noreply.github.com>
2022-10-04 21:43:20 +0200

    Add files via upload

---
+++ animation.js
@@ -0,0 +1,33 @@
+const anim = ScrollReveal({
+    mobile: true,
+    reset : true,
+    duration : 2000,
+
+});
+
+anim.reveal('#title', {
+ 
+});
+
+anim.reveal('#text', {
+    delay : 500,
+
+});
+
+anim.reveal('.card', {
+    delay : 500,
+}, 500);  
+
+anim.reveal('.certf',{
+    delay : 500,
+},50);
+
+anim.reveal('.progress',{
+    delay : 500,
+
+});
+
+anim.reveal('h26',{
+    delay: 500,
+
+})
